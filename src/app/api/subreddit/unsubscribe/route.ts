51ecf24c8e73e04ad6115862a134822b4b5aa21e
Giaco <jacopo.albrizio@gmail.com>
2023-08-05 10:24:43 +0200

    feat: add infinite scrolling

--- src/app/api/subreddit/unsubscribe/route.ts
+++ src/app/api/subreddit/unsubscribe/route.ts
@@ -50,7 +50,7 @@ export async function POST(req: Request) {
         return new Response(subredditId)
     } catch (error) {
         if (error instanceof z.ZodError) {
-            return new Response('Invalid request data passed', { status: 422 })
+            return new Response('Invalid POST request data passed', { status: 422 })
         }
 
         return new Response('Could not unsubscribe, please try again later', { status: 500 })
